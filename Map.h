e8c6a87717a738abcbe9dae828385559f434f86a
Roman <thekukinpower@gmail.com>
2021-02-02 21:17:31 +0300

    feat: testing tree iterator

---
+++ Map.h
@@ -0,0 +1,9 @@
+#pragma once
+//#include <map>
+
+namespace ft {
+	//	------------------------------------- VECTOR ITERATOR -----------------------------------------
+	template<class T>
+	class Map {
+
+	};
