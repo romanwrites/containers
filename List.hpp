f38233a1dcce5fadfb2d373f9ce810678843be7f
kukinpower <thekukinpower@gmail.com>
2021-02-09 23:14:17 +0300

    feat: add basic insert to list

--- List.hpp
+++ List.hpp
@@ -20,6 +20,7 @@ namespace ft {
 			prev = obj.prev;
 		}
 	public:
+
 		bool operator==(const NodeList &rhs) const {
 			return value == rhs.value;
 		}
@@ -61,18 +62,30 @@ namespace ft {
 		typedef std::ptrdiff_t 					difference_type;
 		typedef Iter							value_type;
 		typedef ft::bidirectional_iterator_tag	iterator_category;
-//		typedef value_type& 					reference;
-//		typedef value_type* 					pointer;
+		typedef value_type& 					reference;
+		typedef value_type* 					pointer;
 
 		explicit BidirectionalListIt() throw() : ptr(nullptr) {}
 		explicit BidirectionalListIt(NodeList<Iter> *p) throw() : ptr(p) {}
-		explicit BidirectionalListIt(const NodeList<const Iter> *p) throw() : ptr(const_cast<Node<const Iter> *>(p)) {}
+		explicit BidirectionalListIt(const NodeList<const Iter> *p) throw() : ptr(const_cast<NodeList<const Iter> *>(p)) {}
 		BidirectionalListIt(const BidirectionalListIt& p) throw() : ptr(p.ptr) {}
 
 		BidirectionalListIt& operator=(const BidirectionalListIt& rhs) {
 			ptr = rhs.ptr;
 			return *this;
 		}
+
+		// ? or this ? //todo
+        reference operator*() {
+            return *(this->ptr);
+        }
+
+        pointer operator->() {
+		    return &(this->ptr);
+		}
+
+		// const -> * todo
+
 
 		bool operator==(const BidirectionalListIt &rhs) const {
 			return ptr == rhs.ptr;
@@ -102,6 +115,9 @@ namespace ft {
 	//	------------------------------------- LIST CONTAINER -----------------------------------------
 	template < class T, class Alloc = std::allocator<T> >
     class List {
+    private:
+        friend class NodeList<T>; // todo
+
 	private:
 
         typedef T						value_type;
@@ -113,15 +129,15 @@ namespace ft {
 		typedef ptrdiff_t				difference_type;
 		typedef size_t 					size_type;
 		typedef BidirectionalListIt<T>	iterator;
-//		typedef node_pointer
-//		typedef
-//		typedef
 
 		NodeList<value_type>* shadow;
+		NodeList<value_type>* start;
+        NodeList<value_type>* finish;
+        int currentSize;
 
-		friend class NodeList; // to list todo
+
     public:
-    	List() : shadow(new NodeList<value_type>()) {}
+    	List() : shadow(new NodeList<value_type>()), start(nullptr), finish(nullptr), currentSize(0) {}
 
     	iterator begin() {
     		return shadow->next;
@@ -133,14 +149,43 @@ namespace ft {
 
     	void push_back(value_type const &value) {
     		//todo use insert
-//			NodeList<value_type> *ptr = new NodeList<value_type>();
-//			end->next = ptr;
-//			ptr->prev = end;
-//			end->prev = ptr;
+			insert(finish, value);
     	}
 
-    	void insert(value_type const &value) {
+    	void push_front(value_type const &value) {
+            insert(start, value);
+    	}
+
+    	// single element
+        iterator insert(iterator position, const value_type& val) {
+            NodeList<value_type> *insert_node = new NodeList<value_type>();
+            insert_node->value = val;
+            if (currentSize == 0) {
+                shadow->next = insert_node;
+                shadow->prev = insert_node;
+                insert_node->next = shadow;
+                insert_node->prev = shadow;
+            } else {
+                NodeList<value_type> *pos = position.ptr;
+                insert_node->next = pos;
+                insert_node->prev = pos->prev;
+                insert_node->prev->next = insert_node;
+                insert_node->next->prev = insert_node;
+            }
+            ++currentSize;
+            return iterator(insert_node);
+    	}
+
+    	// fill
+        void insert(iterator position, size_type n, const value_type& val) {
 
     	}
+
+    	// range
+        template <class InputIterator>
+        void insert(iterator position, InputIterator first, InputIterator last) {
+
+    	}
+
 	};
 }
--- main.cpp
+++ main.cpp
@@ -2,146 +2,116 @@
 #include <iostream>
 #include <vector>
 #include <set>
-#include "List.hpp"
+#include <list>
+//#include "List.hpp"
 
 template<typename T>
 void PrintVector(const T& v) {
-	for (size_t i = 0; i < v.size(); i++) {
-		std::cout << v[i] << " ";
-	}
-	std::cout << std::endl << "-------------------------------" << std::endl;
+    for (size_t i = 0; i < v.size(); i++) {
+        std::cout << v[i] << " ";
+    }
+    std::cout << std::endl << "-------------------------------" << std::endl;
 }
 
 void testPushPop() {
-	std::cout << "---------Test push_back, pop--------" << std::endl;
-	ft::Vector<int>		v;
-	for (int i = 1, j = 0; j < 16; i*=2, j++)
-		v.push_back(i);
-	PrintVector(v);
-	v.pop_back();
-	v.pop_back();
-	v.pop_back();
-	PrintVector(v);
+    std::cout << "---------Test push_back, pop--------" << std::endl;
+    ft::Vector<int>		v;
+    for (int i = 1, j = 0; j < 16; i*=2, j++)
+        v.push_back(i);
+    PrintVector(v);
+    v.pop_back();
+    v.pop_back();
+    v.pop_back();
+    PrintVector(v);
 }
 
 void testElemsConstructor() {
-	std::cout << "---------Test elems constructor--------" << std::endl;
-	ft::Vector<int> v(15, 50);
-	PrintVector(v);
-	std::cout << "v size: " << v.size() << std::endl;
-	v.pop_back();
-	v.pop_back();
-	v.pop_back();
-	v.pop_back();
-	v.pop_back();
-	std::cout << "v size: " << v.size() << std::endl;
-	v.push_back(18);
-	v.push_back(18);
-	v.push_back(18);
-	PrintVector(v);
-	std::cout << "v size: " << v.size() << std::endl;
+    std::cout << "---------Test elems constructor--------" << std::endl;
+    ft::Vector<int> v(15, 50);
+    PrintVector(v);
+    std::cout << "v size: " << v.size() << std::endl;
+    v.pop_back();
+    v.pop_back();
+    v.pop_back();
+    v.pop_back();
+    v.pop_back();
+    std::cout << "v size: " << v.size() << std::endl;
+    v.push_back(18);
+    v.push_back(18);
+    v.push_back(18);
+    PrintVector(v);
+    std::cout << "v size: " << v.size() << std::endl;
 }
 
 void testAssign() {
-	std::cout << "---------Test assign--------" << std::endl;
-	ft::Vector<int>		v;
-	std::vector<int>	stl;
-	for (int i = 1, j = 0; j < 16; i*=2, j++)
-		stl.push_back(i);
-	v.assign(stl.begin(), stl.end());
-	PrintVector(v);
+    std::cout << "---------Test assign--------" << std::endl;
+    ft::Vector<int>		v;
+    std::vector<int>	stl;
+    for (int i = 1, j = 0; j < 16; i*=2, j++)
+        stl.push_back(i);
+    v.assign(stl.begin(), stl.end());
+    PrintVector(v);
 }
 
 void testIterator() {
-	std::cout << "---------Test iterators--------" << std::endl;
-	ft::Vector<int>		v;
-	for (int i = 1, j = 0; j < 16; i*=2, j++)
-		v.push_back(i);
-	ft::Vector<int>::iterator it = v.begin();
-	while (it != v.end()) {
-		std::cout << *it << "\t";
-		it++;
-	}
+    std::cout << "---------Test iterators--------" << std::endl;
+    ft::Vector<int>		v;
+    for (int i = 1, j = 0; j < 16; i*=2, j++)
+        v.push_back(i);
+    ft::Vector<int>::iterator it = v.begin();
+    while (it != v.end()) {
+        std::cout << *it << "\t";
+        it++;
+    }
 }
 
 template <class T>
 void printMap(const T& t) {
-	for (typename T::const_iterator it = t.begin(); it != t.end(); ++it) {
-		std::cout << *it << std::endl;
-	}
+    for (typename T::const_iterator it = t.begin(); it != t.end(); ++it) {
+        std::cout << *it << std::endl;
+    }
 }
 
-template <class T>
-void testList() {
-	std::list<int> lst;
-	ft::List<int> ftlst;
-	std::cout << *(lst.begin()) <<std::endl;
-	std::cout << *(ftlst.begin()) <<std::endl;
+//void emptyBegin() {
+//	std::list<int> lst;
+//	ft::List<int> ftlst;
+//	std::cout << *(lst.begin()) <<std::endl;
+//	std::cout << *(ftlst.begin()) <<std::endl;
+//}
+
+void testVector() {
+    testElemsConstructor();
+    testPushPop();
+    testAssign();
+    testIterator();
 }
+
+void testList() {
+    std::list<std::string> lst;
+
+    lst.insert(lst.begin(), "+");
+    lst.insert(lst.begin(), "С");
+    lst.insert(lst.end(), "+");
+
+    for (auto &i : lst) {
+        std::cout << i << std::endl;
+    }
+
+    for (std::list<std::string>::iterator it = lst.begin(); it != lst.end(); it++) {
+        std::cout << *it << " ";
+    }
+    std::cout << std::endl;
+
+
+    std::cout << "================= emptyBegin() ===============\n";
+//    emptyBegin();
+}
+
+
 
 int main() {
+    testList();
+//    testVector();
 
-//	for (std::string::iterator it = str.begin(); it != str.end(); ++it) {
-//		std::cout << *it << std::endl;
-//	}
-
-//	testElemsConstructor();
-//	testPushPop();
-//	testAssign();
-//	testIterator();
     return 0;
 }
-
-
-//int main() {
-//	std::vector<std::string> vv;
-//	try {
-//		vv.at(45);
-//	} catch (std::exception &ex) {
-//		std::cout << ex.what() << std::endl;
-//	}
-//
-//	std::string str = "Hello world\n";
-//	printMap(str);
-//
-//	auto root = std::make_unique<Node<std::string>>("a1", nullptr, nullptr, nullptr);
-//	root->left = std::make_unique<Node<std::string>>("b1", nullptr, nullptr, root.get()); // get raw pointer
-//	root->right = std::make_unique<Node<std::string>>("b2", nullptr, nullptr, root.get());
-//
-//	auto b1 = root->left.get();
-//	auto b2 = root->right.get();
-//	b1->left = std::make_unique<Node<std::string>>("c1", nullptr, nullptr, b1);
-//	b1->right = std::make_unique<Node<std::string>>("c2", nullptr, nullptr, b1);
-//	b2->left = std::make_unique<Node<std::string>>("c3", nullptr, nullptr, b2);
-//	b2->right = std::make_unique<Node<std::string>>("c4", nullptr, nullptr, b2);
-////	auto c1 = b1->left.get();
-////	auto c2 = b1->right.get();
-////	auto c3 = b2->left.get();
-////	auto c4 = b2->right.get();
-//
-//	for (auto it = root->begin(); it != root->end(); ++it) {
-//		std::cout << *it << std::endl;
-//	}
-//
-//	std::cout << std::endl;
-//
-//	for (auto &&node : *root) {
-//		std::cout << node << std::endl;
-//	}
-//
-//	std::set<Point> points{ {5,3}, {1,2}, {0,0}, {3, 2} }; //фигурная инициализация
-//	for (auto &&point : points) {
-//		std::cout << point.x << " " << point.y << std::endl;
-//	}
-//
-//}
-
-//#include <list>
-//int main() {
-//	std::vector<std::string> vv;
-//	try {
-//		vv.at(28);
-//	} catch (std::exception &ex) {
-//		std::cout << ex.what() << "WHAAAAAAT" << std::endl;
-//	}
-//}
