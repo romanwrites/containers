f3d6d77c36b545ba26bc8d56cd2ff588da85383f
kukinpower <thekukinpower@gmail.com>
2021-03-15 18:02:39 +0300

    feat: map add typedefs

--- Map.h
+++ Map.h
@@ -99,6 +99,25 @@ template < class Key,                                           // map::key_type
     class Compare = less<Key>,                                // map::key_compare
     class Alloc = std::allocator<std::pair<const Key,T> >    // map::allocator_type
 > class Map {
-//  todo
+  typedef Key                                       key_type;
+  typedef T                                         mapped_type;
+  typedef std::pair<const key_type,mapped_type>     value_type;
+  typedef Compare                                   key_compare;
+  typedef Alloc                                     allocator_type;
+  typedef typename allocator_type::reference        reference;
+  typedef typename allocator_type::const_reference  const_reference;
+  typedef typename allocator_type::pointer          pointer;
+  typedef typename allocator_type::const_pointer    const_pointer;
+  typedef std::ptrdiff_t                            difference_type;
+  typedef size_t                                    size_type;
+
+  //todo iterator typedefs
+//  typedef mapIterator iterator;
+//  typedef mapIterator const_iterator;
+//  typedef mapIterator reverse_iterator;
+//  typedef mapIterator const_reverse_iterator;
+
+
+//  todo constructors...
 };
 }
