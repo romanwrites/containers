b24e70e2e4bd1c04f12c55acbef02ab1bacc47a4
kukinpower <thekukinpower@gmail.com>
2021-03-16 17:38:04 +0300

    refactor: map node and category

--- Map.h
+++ Map.h
@@ -1,21 +1,21 @@
 #pragma once
 
-//#include <map>
+#include <map>
 
 namespace ft {
 //	------------------------------------- MAP ITERATOR -----------------------------------------
-struct input_iterator_tag {};
 
 template<class T>
 class Node {
  private:
+
   template<class Iter>
   class NodeIterator {
     friend class Node;
 
    public:
     typedef Iter iterator_type;
-    typedef ft::input_iterator_tag iterator_category;
+    typedef std::bidirectional_iterator_tag iterator_category;
     typedef iterator_type value_type;
     typedef ptrdiff_t difference_type;
     typedef iterator_type &reference;
@@ -58,11 +58,11 @@ class Node {
 
  public:
   T value;
-  std::unique_ptr<Node> left;
-  std::unique_ptr<Node> right;
+  Node * left;
+  Node * right;
   Node *parent;
 
-  Node(const T &value, std::unique_ptr<Node> left, std::unique_ptr<Node> right, Node *parent)
+  Node(const T &value, Node * left, Node * right, Node *parent)
       : value(value), left(std::move(left)), right(std::move(right)), parent(parent) {}
   Node(const Node &) = delete;
 
@@ -94,22 +94,23 @@ class Node {
   }
 };
 
-template < class Key,                                           // map::key_type
+template<class Key,                                           // map::key_type
     class T,                                                   // map::mapped_type
     class Compare = less<Key>,                                // map::key_compare
-    class Alloc = std::allocator<std::pair<const Key,T> >    // map::allocator_type
-> class Map {
-  typedef Key                                       key_type;
-  typedef T                                         mapped_type;
-  typedef std::pair<const key_type,mapped_type>     value_type;
-  typedef Compare                                   key_compare;
-  typedef Alloc                                     allocator_type;
-  typedef typename allocator_type::reference        reference;
-  typedef typename allocator_type::const_reference  const_reference;
-  typedef typename allocator_type::pointer          pointer;
-  typedef typename allocator_type::const_pointer    const_pointer;
-  typedef std::ptrdiff_t                            difference_type;
-  typedef size_t                                    size_type;
+    class Alloc = std::allocator<std::pair<const Key, T> >    // map::allocator_type
+>
+class Map {
+  typedef Key key_type;
+  typedef T mapped_type;
+  typedef std::pair<const key_type, mapped_type> value_type;
+  typedef Compare key_compare;
+  typedef Alloc allocator_type;
+  typedef typename allocator_type::reference reference;
+  typedef typename allocator_type::const_reference const_reference;
+  typedef typename allocator_type::pointer pointer;
+  typedef typename allocator_type::const_pointer const_pointer;
+  typedef std::ptrdiff_t difference_type;
+  typedef size_t size_type;
 
   //todo iterator typedefs
 //  typedef mapIterator iterator;
