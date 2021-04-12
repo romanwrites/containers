fec4c67dcca94a7e702049297b14b7f5140756f3
kukinpower <thekukinpower@gmail.com>
2021-04-12 22:19:02 +0300

    add set header

---
+++ Set.h
@@ -0,0 +1,10 @@
+#pragma once
+
+namespace ft {
+template < class T,                        // set::key_type/value_type
+    class Compare = less<T>,        // set::key_compare/value_compare
+    class Alloc = allocator<T>      // set::allocator_type
+> class Set {
+
+};
+}
