348a17f08522f5efdc79421ef983add39c08b9ce
kukinpower <thekukinpower@gmail.com>
2021-04-15 22:19:43 +0300

    finish reverse iterator

--- ReverseIterator.h
+++ ReverseIterator.h
@@ -3,12 +3,12 @@
 
 namespace ft {
 
-template <class Iterator>
+template<class Iterator>
 class reverse_iterator {
  public:
   typedef Iterator iterator_type;
   typedef typename ft::iterator_traits<Iterator>::iterator_category iterator_category;
-  typedef typename ft::iterator_traits<Iterator>::value_type	value_type;
+  typedef typename ft::iterator_traits<Iterator>::value_type value_type;
   typedef typename ft::iterator_traits<Iterator>::difference_type difference_type;
   typedef typename ft::iterator_traits<Iterator>::pointer pointer;
   typedef typename ft::iterator_traits<Iterator>::reference reference;
@@ -20,11 +20,11 @@ class reverse_iterator {
   reverse_iterator() {
   }
 
-  explicit reverse_iterator (iterator_type it) : it(it) {
+  explicit reverse_iterator(iterator_type it) : it(it) {
   }
 
-  template <class Iter>
-  reverse_iterator (const reverse_iterator<Iter>& rev_it) : it(rev_it.base()) {
+  template<class Iter>
+  reverse_iterator(const reverse_iterator<Iter> &rev_it) : it(rev_it.base()) {
   }
 
   iterator_type base() const {
@@ -32,20 +32,102 @@ class reverse_iterator {
   }
 
 // Internally, the function decreases a copy of its base iterator and returns the result of dereferencing it.
+// https://www.cplusplus.com/reference/iterator/reverse_iterator/operator*/
   reference operator*() const {
     iterator_type copy = it;
     --copy;
     return *copy;
   }
 
+  pointer operator->() const {
+    return &(operator*());
+  }
 
+// Internally, the function accesses the proper element of its base iterator, returning the same as: base()[-n-1].
+// https://www.cplusplus.com/reference/iterator/reverse_iterator/operator[]/
+  reference operator[](difference_type n) const {
+    return it[-n - 1];
+  }
 
+//  Returns a reverse iterator pointing to the element located n positions away
+//  from the element the iterator currently points to.
+// https://www.cplusplus.com/reference/iterator/reverse_iterator/operator+/
+  reverse_iterator operator+(difference_type n) const {
+    return reverse_iterator(it - n);
+  }
 
+  reverse_iterator &operator++() {
+    --it;
+    return *this;
+  }
 
+  reverse_iterator operator++(int) {
+    reverse_iterator temp = *this;
+    ++(*this);
+    return temp;
+  }
+
+  reverse_iterator &operator+=(difference_type n) {
+    it -= n;
+    return *this;
+  }
+
+  reverse_iterator operator-(difference_type n) const {
+    return reverse_iterator(it + n);
+  }
+
+  reverse_iterator &operator--() {
+    ++it;
+    return *this;
+  }
+
+  reverse_iterator operator--(int) {
+    reverse_iterator temp = *this;
+    --(*this);
+    return temp;
+  }
+
+  reverse_iterator &operator-=(difference_type n) {
+    it += n;
+    return *this;
+  }
 
 };
 
+template<class Iterator>
+bool operator==(const reverse_iterator<Iterator> &lhs,
+                const reverse_iterator<Iterator> &rhs) {
+  return lhs.base() == rhs.base();
+}
 
+template<class Iterator>
+bool operator!=(const reverse_iterator<Iterator> &lhs,
+                const reverse_iterator<Iterator> &rhs) {
+  return lhs.base() != rhs.base();
+}
 
+template<class Iterator>
+bool operator<(const reverse_iterator<Iterator> &lhs,
+               const reverse_iterator<Iterator> &rhs) {
+  return lhs.base() < rhs.base();
+}
+
+template<class Iterator>
+bool operator<=(const reverse_iterator<Iterator> &lhs,
+                const reverse_iterator<Iterator> &rhs) {
+  return lhs.base() <= rhs.base();
+}
+
+template<class Iterator>
+bool operator>(const reverse_iterator<Iterator> &lhs,
+               const reverse_iterator<Iterator> &rhs) {
+  return lhs.base() > rhs.base();
+}
+
+template<class Iterator>
+bool operator>=(const reverse_iterator<Iterator> &lhs,
+                const reverse_iterator<Iterator> &rhs) {
+  return lhs.base() >= rhs.base();
+}
 
 }
