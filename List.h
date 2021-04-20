2fe006a43de04c699e8b6f706dbc651f6cabe9f4
kukinpower <thekukinpower@gmail.com>
2021-04-20 15:00:31 +0300

    refactor: move compare functions to separate file

--- List.h
+++ List.h
@@ -4,37 +4,9 @@
 #include <list>
 #include "Traits.h"
 #include "ReverseIterator.h"
+#include "Compare.h"
 
 namespace ft {
-
-// -------------------------------------- COMPARE -------------------------------------
-template<class Arg1, class Arg2, class Result>
-struct binary_function {
-  typedef Arg1 first_argument_type;
-  typedef Arg2 second_argument_type;
-  typedef Result result_type;
-};
-
-template<class T>
-struct less : binary_function<T, T, bool> {
-  bool operator()(T const &x, T const &y) const {
-    return x < y;
-  }
-};
-
-template<class T>
-struct greater : binary_function<T, T, bool> {
-  bool operator()(T const &x, T const &y) const {
-    return x > y;
-  }
-};
-
-template<class T>
-struct equal_to : binary_function<T, T, bool> {
-  bool operator()(T const &x, T const &y) const {
-    return x == y;
-  }
-};
 
 // -------------------------------------- TAGS -------------------------------------
 //struct input_iterator_tag {};
--- RedBlackTree.h
+++ RedBlackTree.h
@@ -2,6 +2,7 @@
 
 #include "RbTreeIterator.h"
 #include "Allocator.h"
+#include "Compare.h"
 
 namespace ft {
 
@@ -210,37 +211,6 @@ class RbTree {
       std::cout << print(root, "", true);
     }
   }
-
 };
-
-// -------------------------------------- COMPARE -------------------------------------
-template<class Arg1, class Arg2, class Result>
-struct binary_function {
-  typedef Arg1 first_argument_type;
-  typedef Arg2 second_argument_type;
-  typedef Result result_type;
-};
-
-template<class T>
-struct less : binary_function<T, T, bool> {
-  bool operator()(T const &x, T const &y) const {
-    return x < y;
-  }
-};
-
-template<class T>
-struct greater : binary_function<T, T, bool> {
-  bool operator()(T const &x, T const &y) const {
-    return x > y;
-  }
-};
-
-template<class T>
-struct equal_to : binary_function<T, T, bool> {
-  bool operator()(T const &x, T const &y) const {
-    return x == y;
-  }
-};
-
 
 }
